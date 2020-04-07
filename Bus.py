f6fc8b20358f73c8b326f36d9bb89fa7ebc7a6c0
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-04-07 12:11:19 +0300

    Add files via upload

---
+++ Bus.py
@@ -0,0 +1,10 @@
+def bus(cap,on,wait):
+	"""cap is the amount of people the bus can hold excuding the driver
+	on is the number of people on the bus
+	wait is the number of people waiting to get on to the bus"""
+	print(cap,on,wait)
+	if cap>=on+wait:
+		print ('He can fit all',wait,'passengers and +',cap-on-wait)
+	elif cap<on+wait:
+		print ('He can not fit',on-wait,'of',wait,'waiting')
+bus(110,60,50)
