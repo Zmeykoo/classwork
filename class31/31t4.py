4432066c10fa4c3d316e872c5065424a5f50a175
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-04-09 16:15:38 +0300

    Delete 31t4.py

--- class31/31t4.py
+++
@@ -1,9 +0,0 @@
-env=['Luba','Max','Petro','Angelina','Igor','Vlad','Solomiia']
-lo=str(input('Please, enter login: '))
-
-print (lo)
-print ('type(lo) =',type(lo),'\n')
-if lo in env:
-	print('Your login is true, nice dick...')
-else:
-	print('You are not one of us, slut')
