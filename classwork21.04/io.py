92ed95e35943eed53cbe92a2af721955338926d6
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-04-25 11:48:34 +0300

    Add files via upload

--- classwork21.04/io.py
+++ classwork21.04/io.py
@@ -1,5 +1,10 @@
+#Task3
 ji=['1','2','7','8']
-fi=[int(j) for j in ji]
-si=list(map(lambda i: int(i), ji))
+ri=[]
+for i in ji: # append
+	ri.append(int(i))
+fi=[int(j) for j in ji]#list comprehension
+si=list(map(lambda i: int(i), ji))#map
 
 print(type(si[0]),si,'\n',type(fi[0]),fi)
+print(type(ri),ri)
