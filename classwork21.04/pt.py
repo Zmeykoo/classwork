1b76492ac4e71d61571b13c062da949e5e3bb75f
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-04-25 11:50:02 +0300

    Add files via upload

---
+++ classwork21.04/pt.py
@@ -0,0 +1,8 @@
+#Task 2
+spi=['red','green','black','red','brown','red','blue','red','red','yellow']
+p=list(filter(lambda x: x=='red',spi))
+print (p)
+#OR!
+from collections import Counter
+fik=Counter(spi)
+#ro be countinued
